e9e583f5d6ba9c0f86ddadbe8c7b1be26abd19c2
Rodrigo Alfonso Ríos Quevedo <rodrigo140599@gmail.com>
2024-03-25 00:07:33 -0500

    feat: Dashboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,28 @@
 import { Col, Row } from "react-bootstrap";
 import { InfoBox } from "../components/InfoBox";
 import { BookmarkStarFill, TagsFill } from "react-bootstrap-icons";
+import { useEffect, useState } from "react";
 
 export const HomePage = () => {
+
+  const urlApi = import.meta.env.VITE_API_URL;
+
+  const [dashboardData, setDashboardData] = useState([]);
+
+  const fetchDashboardData = async () => {
+    try {
+      const response = await fetch(`${urlApi}dashboard`);
+      const data = await response.json();
+      setDashboardData(data.data);
+    } catch (error) {
+      console.error('Error fetching dashboard data: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
   return (
     <Row className='justify-content-center mt-3'>
       <Col sm={12} md={3}>
@@ -12,7 +32,7 @@ export const HomePage = () => {
           iconColor='white'
           iconBg='success'
           title='Total Categories'
-          content='10'
+          content={dashboardData.category_count || 0}
         />
       </Col>
       <Col sm={12} md={3}>
@@ -22,7 +42,7 @@ export const HomePage = () => {
           iconColor='dark'
           iconBg='warning'
           title='Total Products'
-          content='10'
+          content={dashboardData.product_count || 0}
         />
       </Col>
     </Row>
